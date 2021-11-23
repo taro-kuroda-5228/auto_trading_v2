ad5a0f9bc0eccdbf6c80a74b36e09b4dbe03791e
taro-kuroda-5228 <90184590+taro-kuroda-5228@users.noreply.github.com>
2021-11-23 14:25:47 +0900

    Add: main.py (#7)
    * Add: main.py
    
    * Update: main.py
    
    * Update: main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+from auto_trading.datamart import Datamart
+from auto_trading.name import Name
+from auto_trading.raw_data import RawData
+from auto_trading.symbol_data import SymbolData
+
+def main():
+  ticker = Name("MSFT", "US").ticker
+  symbol_data = SymbolData(ticker).symbol_data
+  raw_data = RawData(symbol_data).raw_data
+  datamart = Datamart(raw_data, "close", 5)
+  print(datamart.datamart.head())
+
+if __name__ == "__main__":
+  main()
