a070a4f599976c423ce13f8923e14a1a6a827248
taro-kuroda-5228 <90184590+taro-kuroda-5228@users.noreply.github.com>
2021-12-07 19:31:07 +0900

    Update: test_name_py_2 (#13)
    * Update: test_name_py_2
    
    * Update: test_name.py

--- tests/auto_trading/test_name.py
+++ tests/auto_trading/test_name.py
@@ -1,6 +1,18 @@
+import pytest
+
 from auto_trading.name import Name
 
-def test_name():
-    actual = Name("MSFT", "US").ticker
-    expected = "MSFT"
+
+def test_name_class():
+    assert Name("MSFT", "US")
+
+
+@pytest.mark.parametrize(
+    "company_ticker,nation,ticker_expected",
+    [("MSFT", "US", "MSFT"), ("7203", "JP", "7203.T")],
+    )
+def test_name(company_ticker, nation, ticker_expected):
+    ticker = Name(company_ticker, nation).ticker
+    actual = ticker
+    expected = ticker_expected
     assert actual == expected
