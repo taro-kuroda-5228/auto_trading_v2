5c7c02f528aff3a7b31d7a7f67b2770fe6a85ba1
atsushimemet <noap3b69n@gmail.com>
2021-12-14 09:28:00 +0900

    Fix: Lint (#17)

--- tests/auto_trading/test_name.py
+++ tests/auto_trading/test_name.py
@@ -10,7 +10,7 @@ def test_name_class():
 @pytest.mark.parametrize(
     "company_ticker,nation,ticker_expected",
     [("MSFT", "US", "MSFT"), ("7203", "JP", "7203.T")],
-    )
+)
 def test_name(company_ticker, nation, ticker_expected):
     ticker = Name(company_ticker, nation).ticker
     actual = ticker
--- tests/auto_trading/test_symbol_data.py
+++ tests/auto_trading/test_symbol_data.py
@@ -1,25 +1,26 @@
-import pytest
-
 from auto_trading.symbol_data import SymbolData
 
 
 def test_symbol_data_class():
-  assert SymbolData('MSFT')
+    assert SymbolData("MSFT")
+
 
 def symbol_data_type():
-  type = type(SymbolData.symbol_data)
-  actual = type
-  type_expected = dict
-  assert actual == type_expected
+    type_symbol_data = type(SymbolData.symbol_data)
+    actual = type_symbol_data
+    type_expected = dict
+    assert actual == type_expected
+
 
 def symbol_data_keys():
-  keys = list(SymbolData.symbol_data.keys())
-  actual = keys
-  keys_expected = ["timestamp", "open", "high", "low", "close", "volume"]
-  assert actual == keys_expected
+    keys = list(SymbolData.symbol_data.keys())
+    actual = keys
+    keys_expected = ["timestamp", "open", "high", "low", "close", "volume"]
+    assert actual == keys_expected
+
 
 def symbol_data_data_n():
-  data_n = len(SymbolData.symbol_data["timestamp"])
-  actual = data_n
-  data_n_expected = 286
-  assert actual <= data_n_expected
+    data_n = len(SymbolData.symbol_data["timestamp"])
+    actual = data_n
+    data_n_expected = 286
+    assert actual <= data_n_expected
