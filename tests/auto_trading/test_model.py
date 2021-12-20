a75bcc839b071ddb597d179ae72387e8f29c214f
taro-kuroda-5228 <dancing205228@gmail.com>
2021-12-20 23:00:49 +0900

    Update: test_model.py

--- tests/auto_trading/test_model.py
+++ tests/auto_trading/test_model.py
@@ -1,10 +1,18 @@
 import pytest
 
 from auto_trading.model import Model
+from auto_trading.name import Name
+from auto_trading.symbol_data import SymbolData
 from auto_trading.datamart import Datamart
+from auto_trading.raw_data import RawData
 
-datamart = Datamart.datamart
+ticker_msft = Name("MSFT", "US").ticker
+symbol_data_msft = SymbolData(ticker_msft).symbol_data
+raw_data_msft = RawData(symbol_data_msft).raw_data
+datamart = Datamart(raw_data_msft, "open", 5).datamart
+
+model = Model(datamart)
 
 
-def test_model_class():
-    assert Model(datamart)._datamart
+def test_model_instance__datamart():
+    assert all(model._datamart == datamart)
