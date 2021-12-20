3c9b5f473b3f82798855f0a9929d57f7330d9c77
taro-kuroda-5228 <dancing205228@gmail.com>
2021-12-20 21:20:22 +0900

    Update: model and test_model files

--- auto_trading/model.py
+++ auto_trading/model.py
@@ -1,2 +1,3 @@
 class Model:
-    pass
+    def __init__(self, datamart):
+        self._datamart = datamart
--- tests/auto_trading/test_model.py
+++ tests/auto_trading/test_model.py
@@ -1,6 +1,10 @@
 import pytest
+
 from auto_trading.model import Model
+from auto_trading.datamart import Datamart
+
+datamart = Datamart.datamart
 
 
 def test_model_class():
-    assert Model()
+    assert Model(datamart)._datamart
