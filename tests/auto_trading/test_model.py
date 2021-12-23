fa4a507aadc3e0f118ce308076b6684c9b2b9cfe
taro-kuroda-5228 <90184590+taro-kuroda-5228@users.noreply.github.com>
2021-12-23 21:32:50 +0900

    Update: model and test_model files (#21)
    * Update: model and test_model files
    
    * Update: test_model.py
    
    * Update: test_model.py
    
    * Update: test_model.py
    
    * Update: test_model.py

--- tests/auto_trading/test_model.py
+++ tests/auto_trading/test_model.py
@@ -1,6 +1,21 @@
 import pytest
+import pandas
+from pandas._testing import assert_frame_equal
+
 from auto_trading.model import Model
+from auto_trading.name import Name
+from auto_trading.symbol_data import SymbolData
+from auto_trading.datamart import Datamart
+from auto_trading.raw_data import RawData
 
 
-def test_model_class():
-    assert Model()
+ticker_msft = Name("MSFT", "US").ticker
+symbol_data_msft = SymbolData(ticker_msft).symbol_data
+raw_data_msft = RawData(symbol_data_msft).raw_data
+datamart = Datamart(raw_data_msft, "open", 5).datamart
+
+model = Model(datamart)
+
+
+def test_model_instance_datamart():
+    assert_frame_equal(model._datamart, datamart)
