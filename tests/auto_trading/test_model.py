c472de1640e2c06af05aeb5f6e2dbaec66ab02b8
taro-kuroda-5228 <dancing205228@gmail.com>
2021-12-27 21:42:45 +0900

    Update:test_model.py

--- tests/auto_trading/test_model.py
+++ tests/auto_trading/test_model.py
@@ -11,18 +11,18 @@ from auto_trading.datamart import Datamart
 from auto_trading.raw_data import RawData
 
 
-ticker_msft = Name("MSFT", "US").ticker
-symbol_data_msft = SymbolData(ticker_msft).symbol_data
-raw_data_msft = RawData(symbol_data_msft).raw_data
-datamart = Datamart(raw_data_msft, "open", 5).datamart
+class TestModelInstance:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        ticker_msft = Name("MSFT", "US").ticker
+        symbol_data_msft = SymbolData(ticker_msft).symbol_data
+        raw_data_msft = RawData(symbol_data_msft).raw_data
+        self.datamart = Datamart(raw_data_msft, "open", 5).datamart
+        self.model = Model(self.datamart)
 
-model = Model(datamart)
+    def test_model_instance_datamart(self):
+        assert_frame_equal(self.model._datamart, self.datamart)
 
-
-def test_model_instance_datamart():
-    assert_frame_equal(model._datamart, datamart)
-
-
-def test_model_instance_fit_clf():
-    model.fit()
-    assert model.clf
+    def test_model_instance_fit_clf(self):
+        self.model.fit()
+        assert self.model.clf
