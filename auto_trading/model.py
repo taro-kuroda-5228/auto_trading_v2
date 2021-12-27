a923bc102dd57d4e142ae15f0fba497f945b47b7
taro-kuroda-5228 <90184590+taro-kuroda-5228@users.noreply.github.com>
2021-12-28 08:03:07 +0900

    Update:model.py and test_model.py (#25)
    * Update:model.py and test_model.py
    
    * Update:model files
    
    * Update:test_model.py
    
    * Update:test_model.py

--- auto_trading/model.py
+++ auto_trading/model.py
@@ -1,3 +1,17 @@
+import lightgbm as lgb
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
 class Model:
     def __init__(self, datamart):
         self._datamart = datamart
+
+    def fit(self):
+        self.clf = lgb.LGBMClassifier()
+        self.df = pd.DataFrame(self._datamart)
+        self.X = self.df[
+            ["open_N-0", "open_N-1", "open_N-2", "open_N-3", "open_N-4", "open_N-5"]
+        ]
+        self.y = self.df["target"]
+        self.clf.fit(self.X, self.y)
--- tests/auto_trading/test_model.py
+++ tests/auto_trading/test_model.py
@@ -1,6 +1,8 @@
 import pytest
 import pandas
+import lightgbm as lgb
 from pandas._testing import assert_frame_equal
+from sklearn.model_selection import train_test_split
 
 from auto_trading.model import Model
 from auto_trading.name import Name
@@ -9,13 +11,18 @@ from auto_trading.datamart import Datamart
 from auto_trading.raw_data import RawData
 
 
-ticker_msft = Name("MSFT", "US").ticker
-symbol_data_msft = SymbolData(ticker_msft).symbol_data
-raw_data_msft = RawData(symbol_data_msft).raw_data
-datamart = Datamart(raw_data_msft, "open", 5).datamart
+class TestModelInstance:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        ticker_msft = Name("MSFT", "US").ticker
+        symbol_data_msft = SymbolData(ticker_msft).symbol_data
+        raw_data_msft = RawData(symbol_data_msft).raw_data
+        self.datamart = Datamart(raw_data_msft, "open", 5).datamart
+        self.model = Model(self.datamart)
 
-model = Model(datamart)
+    def test_model_instance_datamart(self):
+        assert_frame_equal(self.model._datamart, self.datamart)
 
-
-def test_model_instance_datamart():
-    assert_frame_equal(model._datamart, datamart)
+    def test_model_instance_fit_clf(self):
+        self.model.fit()
+        assert self.model.clf
